4e589659a137d0bfb900bb533842b5936681b9b5
koteswar-kots <kotsmaddhur@gmail.com>
2024-12-16 20:18:26 +0530

    add dynamic port

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const app = express()
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI)
